b57f549f88de5f8e828dfbb0caf91f1eebef0ec2
Oğuzhan Uluuğur <oguzhanuluugur345@gmail.com>
2023-02-11 12:14:25 +0300

    patikadevödev
    patikdade c# ödevi

---
+++ PatikaDev_Ödev/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace PatikaDev_Ödev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Soru 
+
+            //Console.WriteLine("Bir Sayı giriniz : ");
+            //int sayi=int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Adet Sayısı giriniz : ");
+            //int sayi2 = int.Parse(Console.ReadLine());
+            //for (int i = sayi; i < sayi2; i++)
+            //{
+            //    if(i%2==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            // 2. Soru 
+
+            //Console.WriteLine("1.Sayıyı giriniz : ");
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine("2.Sayıyı giriniz : ");
+            //int m = int.Parse(Console.ReadLine());
+            //if(n==m || n%m==0)
+            //{
+            //    Console.Write(n);
+            //}
+
+            //Console.WriteLine("Bir Sayı Giriniz : ");
+            //int sayi = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Bir Kelime Giriniz : ");
+
+            //3.soru 
+
+            //Console.WriteLine("Pozitif Bir Sayi Girin : ");
+            //int sayi = int.Parse(Console.ReadLine());
+            //if(sayi<=0)
+            //{
+            //    Console.WriteLine("Lütfen pozitif bir sayı girin : ");
+            //    sayi = int.Parse(Console.ReadLine());
+            //}
+
+
+            //string[] kelimeler = new string[sayi];
+            //for (int i = 1; i <= sayi; i++)
+            //{
+            //    Console.WriteLine(i + " .Kelimeyi Giriniz  :  ");
+            //    string sayi2 = Console.ReadLine();
+            //    kelimeler[i] = sayi2;
+
+
+
+            //}
+            //Array.Reverse(kelimeler);
+            //foreach (var item in kelimeler)
+            //{
+
+            //    Console.WriteLine(item);
+            //}
+            //Console.ReadLine();
+
+
+            //4.soru
+            //Console.WriteLine("Cümle yazınız.");
+            //string input = Console.ReadLine();
+            //string[] words = input.Split(' ');
+            //int w = words.Count();
+            //char[] c = input.Replace(" ", string.Empty).ToCharArray();
+            //int c1 = c.Count();
+            //Console.WriteLine("Kelime sayısı: {0},Harf sayısı: {1}", w, c1);
+            //Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
